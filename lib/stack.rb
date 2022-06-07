9400b8ac80f4ee83338edaa8e9c444d042d8b43b
Calypso Leonard <calypsorleonard@gmail.com>
2022-06-07 13:55:41 -0400

    tests all pass

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,43 @@
 # your code here
+class Stack
+    # attr_accessor @stack
+    # attr_reader 
+    def initialize (limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push element    
+        raise 'Stack Overflow' if @limit && @stack.size == @limit
+
+        @stack.push(element)
+    end
+
+    def pop
+        @stack.pop()
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.length == 0
+    end
+
+    def full?
+        @limit && @stack.size == @limit
+    end
+
+    def search value
+        @stack.each_with_index do |item, index|
+            return size - index - 1 if item == value
+        end
+        -1
+    end
+
+end
